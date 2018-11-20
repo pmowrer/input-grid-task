ce85a0ba63643d07ab4664e41c9b8f846a7f5ec4
Patrick Mowrer <pmowrer@gmail.com>
2018-11-20 16:44:33 -0500

    feat: basic input grid skeleton functionality
    This version allows entering numbers in each of the grid's input
    fields and see the sum calculated in the last grid item. It does
    not yet format the sum to 3 digits. Any non-numerical input is
    treated as 0.

--- summing-grid/src/App.js
+++ summing-grid/src/App.js
@@ -2,12 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      values: [0, 0, 0],
+    };
+  }
+
+  get sum() {
+    return this.state.values.reduce((sum, value) => {
+      const number = Number(value);
+      // If a value contains non-numeric input (NaN), treat it like 0
+      // to prevent the sum from being NaN as well.
+      sum += !isNaN(number) ? number : 0;
+      return sum;
+    }, 0);
+  }
+
+  updateValue(index, event) {
+    const { value } = event.target;
+
+    this.setState(state => {
+      const { values } = state;
+
+      return {
+        values: [...values.slice(0, index), value, ...values.slice(index + 1)],
+      };
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="Grid">
+          {this.state.values.map((value, index) => (
+            <div className="GridItem">
+              <input
+                value={value}
+                key={index}
+                onChange={this.updateValue.bind(this, index)}
+              />
+            </div>
+          ))}
+          <div className="GridItem">
+            <span>{this.sum}</span>
+          </div>
+        </div>
       </div>
     );
   }
